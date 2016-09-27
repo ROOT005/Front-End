2c54ae9121eea05c8802fa76f57b7b3ad361c98d
aptx-05 <1726690348@qq.com>
2016-09-27 17:55:41 +0800

    scroll flow

--- flowlayout/js/script.js
+++ flowlayout/js/script.js
@@ -1,5 +1,34 @@
 window.onload = function() {
     waterfall('main', 'box');
+    //使用json
+    var dataInt = {
+        "data":[
+            {"src":"1.jpg"},
+            {"src":"2.jpg"},
+            {"src":"3.jpg"},
+            {"src":"4.jpg"},
+            {"src":"5.jpg"},
+            {"src":"6.jpg"},
+        ]
+    }
+    window.onscroll = function(){
+        if(checkScrollSlide()){
+            var oParent = document.getElementById('main');
+            //将数据块渲染到页面的尾部
+            for(var i = 0; i < dataInt.data.length; i++){
+                var oBox = document.createElement('div');
+                var oPic = document.createElement('div');
+                var oImg = document.createElement('img');
+                oPic.className = 'pic';
+                oBox.className = 'box';
+                oImg.src = "images/" + dataInt.data[i].src;
+                oParent.appendChild(oBox);
+                oBox.appendChild(oPic);
+                oPic.appendChild(oImg);
+            }
+            waterfall('main', 'box');
+        }
+    }
 }
 function waterfall( parent, box){
     //将main下的所有class为box的元素取出来。
@@ -23,7 +52,7 @@ function waterfall( parent, box){
             oBoxs[i].style.left = oBoxs[index].offsetLeft + 'px';
             hArr[index] += oBoxs[i].offsetHeight;
         }
-        console.log(oBoxs[4].offsetHeight);
+        //console.log(oBoxs[4].offsetHeight);
     }
 }
 //byclass获取元素
@@ -37,4 +66,15 @@ function getByClass(parent, className){
     }
     return boxArr;
 }
-//获取最小高度的索引
+
+//检测是否具备了滚条数据块的条件
+function checkScrollSlide(){
+    var oParent = document.getElementById('main');
+    var oBoxs = getByClass(oParent,'box');
+    var lastBoxH = oBoxs[oBoxs.length - 1].offsetTop+Math.floor(oBoxs[oBoxs.length-1].offsetHeight/2);
+    var scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.body.clientHeight || document.documentElement.clientHeight;
+    //console.log("最后图片偏移"+lastBoxH);
+    //console.log("底边偏移"+(scrollTop+height));
+    return (lastBoxH < scrollTop+height) ? true:false;
+}
